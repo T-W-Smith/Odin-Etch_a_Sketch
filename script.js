c950ae7faf849371cb68b428960b79a602d09c99
T-W-Smith <slicksmith14@gmail.com>
2023-01-27 15:31:17 -0500

    Add an RGB and Grayscale coloring option

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let gridSize = 16;
 
+let isRGB = false;
+let isGrayScale = false;
+
 let isMouseDown = false;
 document.body.onmousedown = () => (isMouseDown = true)
 document.body.onmouseup = () => (isMouseDown = false)
 
 const grids = document.getElementById('grids');
 const sizeBtn = document.getElementById('sizeBtn');
+const rgbBtn = document.getElementById('rgbBtn');
+const grayScaleBtn = document.getElementById('grayScaleBtn');
 
 sizeBtn.addEventListener('click', changeGridSize);
+rgbBtn.addEventListener('click', activateRGBMode);
+grayScaleBtn.addEventListener('click', activateGrayScaleMode);
 
 function displayGrid() {
     grids.style.gridTemplateColumns = 'repeat(' + gridSize + ', 2fr)';
@@ -30,13 +37,29 @@ function removeGrid() {
 }
 
 function changeColors(e) {
-    e.target.style.backgroundColor = 'black';
+    if (isRGB) {
+        e.target.style.backgroundColor = rgbMode();
+    }
+    else if (isGrayScale) {
+        e.target.style.backgroundColor = grayScaleMode(getComputedStyle(e.target).getPropertyValue('background-color'));
+    }
+    else {
+        e.target.style.backgroundColor = 'black';
+    }
 }
 
 function changeColorsHover(e) {
     if (isMouseDown)
     {
-        e.target.style.backgroundColor = 'black';
+        if (isRGB) {
+            e.target.style.backgroundColor = rgbMode();
+        }
+        else if (isGrayScale) {
+            e.target.style.backgroundColor = grayScaleMode(getComputedStyle(e.target).getPropertyValue('background-color'));
+        }
+        else {
+            e.target.style.backgroundColor = 'black';
+        }
     }
 }
 
@@ -55,6 +78,42 @@ function changeGridSize() {
         removeGrid();
         displayGrid();
     }
+}
+
+function activateRGBMode() {
+    if (!isRGB)
+        isRGB = true;
+    else
+        isRGB = false;
+}
+
+function rgbMode() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let newRGB = 'rgb(' + r + ',' + g + ',' + b + ')';
+    return newRGB;
+}
+
+function activateGrayScaleMode() {
+    if (!isGrayScale)
+        isGrayScale = true;
+    else
+        isGrayScale = false;
+}
+
+function grayScaleMode(color) {
+    let colorArr = color.slice(
+        color.indexOf("(") + 1, 
+        color.indexOf(")")
+    ).split(", ");
+    
+    let r = Math.min(Math.max(+colorArr[0] + 26, 0), 255);
+    let g = Math.min(Math.max(+colorArr[1] + 26, 0), 255);
+    let b = Math.min(Math.max(+colorArr[2] + 26, 0), 255);
+
+    let newRGB = 'rgb(' + r + ',' + g + ',' + b + ')';
+    return newRGB;
 }
 
 displayGrid();
